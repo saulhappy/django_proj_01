8fd44b649ee907cf030f9e153439dbe58cf7394b
saulhappy <646634@gmail.com>
2022-01-04 10:01:09 -0600

    hello world msg running

--- products/views.py
+++ products/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    return HttpResponse("HEY, SAUL!")
