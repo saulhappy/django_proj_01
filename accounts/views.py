0dbe0e39421db615cf4be75cbb433dbec2ca67b5
saulhappy <646634@gmail.com>
2022-01-20 09:42:26 -0600

    added login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+from .forms import LoginForm
+
+def login_view(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user != None:
+            login(request, user)
+            return redirect("/")
+        else:
+            request.session['invalid_user'] = 1
+    return render(request, "forms.html", {"form": form})
+
+def logout_view(request):
+    pass
