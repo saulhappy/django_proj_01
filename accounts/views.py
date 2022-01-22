4be6726e87ec28e1e9763c8bcd6c784c45689b8d
saulhappy <646634@gmail.com>
2022-01-22 10:31:46 -0600

    register view

--- accounts/views.py
+++ accounts/views.py
@@ -3,15 +3,30 @@ from django.shortcuts import redirect, render
 
 from .forms import LoginForm, RegisterForm
 
-# Create your views here.
+
+def register_view(request):
+    form = RegisterForm(request.post or None)
+    if form.is_valid():
+        email = form.cleaned_data.get("email")
+        password1 = form.cleaned_data.get("password1")
+        password2 = form.cleaned_data.get("password2")
+        try:
+            user = User.objects.create_user(email, password1)
+        except:
+            user = None
+        if user:
+            login(request, user)
+        else:
+            request.session['register_error'] = 1
+    return render(request, "forms.html", {"form": form})
 
 
 def login_view(request):
     form = LoginForm(request.POST or None)
     if form.is_valid():
-        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
         password = form.cleaned_data.get("password")
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
         if user != None:
             login(request, user)
             return redirect("/")
