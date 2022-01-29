2b6cab1d87087c0bca53a95bb9f15470f9af6650
saulhappy <646634@gmail.com>
2022-01-29 03:34:01 -0600

    starting out user unit testing

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+
+User = get_user_model()
+
+class UserTestCase(TestCase):
+    
+    def setUp(self):
+        user1 =  User(username="saul")
+        user1.is_staff = True
+        user1.is_superuser = True
+        user1.set_password("saul")
+        user1.save()
+
