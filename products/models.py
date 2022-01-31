af2f85d3966060c2bb6a290e5431f3409ae6b328
saulhappy <646634@gmail.com>
2022-01-31 13:24:34 -0600

    another version of categories

--- products/models.py
+++ products/models.py
@@ -6,40 +6,25 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 
 class ProductCategory(models.Model):
-    MARKETING_ANALYTICS = 'marketing_analytics'
-    SALES_ANALYTICS = 'sales_analytics'
-    ENGINEERING_ANALYTICS = 'engineering_analytics'
-    WEB_ANALYTICS = 'web_analytics'
-
-    CATEGORY_CHOICES = (
-        (MARKETING_ANALYTICS, 'Marketing Analytics'),
-        (SALES_ANALYTICS, 'Sales Analytics'),
-        (ENGINEERING_ANALYTICS, 'Engineering Analytics'),
-        (WEB_ANALYTICS, 'Web Analytics')
-    )
-
-    category_name = models.CharField(max_length=140, choices=CATEGORY_CHOICES, null=True, blank=True)
-
-    def __str__(self):
-        return self.category_name
-
-class Product(ProductCategory):
-    def get_or_create_default_category():
-        obj, created = ProductCategory.objects.get_or_create(category_name='marketing_analytics')
-        return obj
-
-    CATEGORY2_CHOICES = (
+    CATEGORY_NAME_CHOICES = (
         ('Marketing Analytics', 'Marketing Analytics'),
         ('Sales Analytics', 'Sales Analytics'),
         ('Engineering Analytics', 'Engineering Analytics'),
-        ('Web Analytics', 'Web Analytics')
+        ('Web Analytics', 'Web Analytics'),
+        ('Cereal', 'Cereal')
     )
+
+    name = models.CharField(max_length=140, choices=CATEGORY_NAME_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(ProductCategory):
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     product_name = models.CharField(max_length=140)
     price = models.IntegerField(default=0)
     slogan = models.TextField(null=True, blank=True)
-    category = models.ForeignKey(ProductCategory, null=True, blank=True, on_delete=models.SET_NULL, related_name='product_category', default=get_or_create_default_category)
-    category2 = models.CharField(max_length=140, choices=CATEGORY2_CHOICES, null=True, blank=True)
+    category_name = models.CharField(max_length=140, choices=ProductCategory.CATEGORY_NAME_CHOICES, null=True, blank=True, default='Cereal')
 
     def __str__(self):
         return self.product_name
