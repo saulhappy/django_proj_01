a8a392236053bb374f796cf7ebcf2b6a330d2ba9
saulhappy <646634@gmail.com>
2022-02-02 14:10:24 -0600

    update user create test

--- accounts/tests.py
+++ accounts/tests.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class UserTestCase(TestCase):
     
     def setUp(self):
-        user1 =  User(username="saul")
+        user1 =  User(email="saul@saul.com")
         user1.is_staff = True
         user1.is_superuser = True
         user1.set_password("saul")
@@ -16,4 +16,7 @@ class UserTestCase(TestCase):
 
     def test_user_create(self):
         user_count = User.objects.all().count()
+        user_qs = User.objects.filter(email__iexact="saul@saul.com")
+        user_exists = user_qs.exists()
+        self.assertTrue(user_exists)
         self.assertEqual(user_count, 1) 
