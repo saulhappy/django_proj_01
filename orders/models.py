00e5c5d18a04e71d7f3cbcdf63d4c813a9fb7673
saulhappy <646634@gmail.com>
2022-02-06 12:38:13 -0600

    fill out orders, made migration

--- orders/models.py
+++ orders/models.py
@@ -7,5 +7,23 @@ from products.models import Product
 
 User = get_user_model()
 
+ORDER_STATUS_CHOICES = (
+    ('created', 'Created'),
+    ('paid', 'Paid'),
+    ('shipped', 'Shipped'),
+    ('completed', 'Completed'),
+    ('refunded', 'Refunded')
+)
+
 class Order(models.Model):
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=50, choices=ORDER_STATUS_CHOICES, default='created')
+    sub_total = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    tax = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    discount = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    total = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    paid = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    shipping_address = models.TextField(blank=True, null=True)
+    billing_address = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
