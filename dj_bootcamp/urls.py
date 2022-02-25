aada7b5b52d201fb78846c3af3cec5701b6343c4
saulhappy <646634@gmail.com>
2022-02-25 01:38:04 -0600

    update urls to include static files

--- dj_bootcamp/urls.py
+++ dj_bootcamp/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 from accounts.views import login_view, logout_view, register_view
@@ -38,3 +40,7 @@ urlpatterns = [
     path('api/products/<int:id>/', product_api_detail_view),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
