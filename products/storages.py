bebbb261551cead3b8ab0059141bc5e521d4b27a
saulhappy <646634@gmail.com>
2022-03-04 17:49:50 -0600

    add ProtectedStorages class

--- products/storages.py
+++ products/storages.py
@@ -1,6 +1,10 @@
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.core.exceptions import ImproperlyConfigured
 
 PROTECTED_MEDIA = getattr(settings, 'PROTECTED_MEDIA', None)
 
 if not PROTECTED_MEDIA: raise ImproperlyConfigured("PROTECTED_MEDIA not set in settings.py")
+
+class ProtectedStorages(FileSystemStorage):
+    location = PROTECTED_MEDIA
