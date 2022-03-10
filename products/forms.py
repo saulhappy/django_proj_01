ff49e920c4e01c53dd8f33f0b93c69b82e294ab2
saulhappy <646634@gmail.com>
2022-03-10 14:49:26 -0600

    update download_order_media method

--- products/forms.py
+++ products/forms.py
@@ -1,5 +1,8 @@
+import pathlib
+from wsgiref.util import FileWrapper
+from mimetypes import guess_type
 from django import forms
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from products.models import Product
 
 # class ProductForm(forms.Form):
@@ -25,8 +28,28 @@ class ProductModelForm(forms.ModelForm):
         return data
 
     def download_order_media(request, *args, **kwargs):
-        qs = Product.objects.all()
+        qs = Product.objects.filter(media__isnull=False)
         product_obj = qs.first()
+
         if not product_obj.media: raise Http404
+        
+        media_path = product_obj.media.path
+        media_path = pathlib.Path(media_path)
+        ext = media_path.suffix
+        pk = product_obj.pk
+        file_name = f"media_{pk}{ext}"
+
+        if not media_path.exists(): raise Http404
+
+        with open(media_path, 'rb') as f:
+            wrapper = FileWrapper(f)
+            content_type = 'application/force-download'
+            guessed_media_type = guess_type(media_path)[0]
+            if guessed_media_type:
+                content_type = guessed_media_type
+            response =  HttpResponse(wrapper, content_type=content_type)
+
+        return HttpResponse
+
 
         
